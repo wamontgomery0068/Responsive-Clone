b7d5ba68e668076916aece3a93557f30143c77e9
Your Name <wamontgomery0068@gmail.com>
2019-01-07 15:27:33 -0600

    Finished the base outline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className = "App">
+        <div className = "Header">
+          <div className = "Nav_Header">
+            <div className = "Nav_Logo">
+              <h3 className = "Logo_Text"> Start Bootstrap </h3>
+            </div>
+            <div className = "Nav_Links">
+              <p className = "Link_Text"> SERVICES </p>
+              <p className = "Link_Text"> PORTFOLIO </p>
+              <p className = "Link_Text"> ABOUT </p>
+              <p className = "Link_Text"> TEAM </p>
+              <p className = "Link_Text"> CONTACT </p>
+            </div>
+          </div>
+          <div className = "Content_Header">
+            <div className = "Content_Welcome">
+              <p className = "Welcome_Text"> Welcome To Our Studio!</p>
+            </div>
+            <div className = "Content_Message">
+              <p className = "Message_Text"> IT"S NICE TO MEET YOU </p>
+            </div>
+            <div className = "Content_Button"></div>
+          </div>
+        </div>
       </div>
     );
   }
